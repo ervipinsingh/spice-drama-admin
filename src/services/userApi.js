378dc12436654b310cb25f0681d4723da9c7fbd1
Atul Verma <atulverma0225@gmail.com>
2026-02-10 02:48:13 +0530

    Fix AuthContext to use adminApi

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_USER_API || "http://localhost:5000";
+
 const userApi = axios.create({
-  baseURL: `${import.meta.env.VITE_USER_API}/api`,
+  baseURL: `${BASE_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // ATTACH TOKEN
 userApi.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 export default userApi;
